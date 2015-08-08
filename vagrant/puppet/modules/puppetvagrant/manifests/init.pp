835f8b7516e8e2d211f2fc3422ff1a1337df7277
Toni Schmidbauer <toni@stderr.at>
2015-08-08 22:03:39 +0200

    removed solaris support

--- vagrant/puppet/modules/puppetvagrant/manifests/init.pp
+++ vagrant/puppet/modules/puppetvagrant/manifests/init.pp
@@ -6,31 +6,13 @@
 #
 
 class puppetvagrant {
+  $vagrant_home = '/home/vagrant'
+  $root_home    = '/root'
 
-  case $::osfamily {
-    'Solaris': {
-      $vagrant_home = '/export/home/vagrant'
-      $root_home    = '/'
-      case $::kernelrelease {
-        '5.11': {
-          require sol11
-          Class['sol11'] -> Class['common']
-        }
-        '5.10': {
-          require sol10
-          Class['sol10'] -> Class['common']
-        }
-      }
+  include rhel
+  include rvm
 
-    }
-    'RedHat': {
-      require rhel
-      $vagrant_home = '/home/vagrant'
-      $root_home    = '/root'
-
-      Class['rhel'] -> Class['common']
-    }
-  }
+  Class['rhel'] -> Class['common'] -> Class['rvm']
 
   class {'common':
     vagrant_home => $vagrant_home,
