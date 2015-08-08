45d9ad953c18517577539eea36bb35fc099584f3
Toni Schmidbauer <toni@stderr.at>
2015-08-08 20:09:29 +0200

    import rvm config

---
+++ vagrant/puppet/modules/puppetvagrant/manifests/rvm.pp
@@ -0,0 +1,42 @@
+class puppetvagrant::rvm {
+  class { '::rvm':
+    proxy_url    => $::http_proxy
+  }
+
+  rvm::system_user { vagrant: }
+
+  rvm_system_ruby {
+    'ruby-2.1.6':
+      ensure      => 'present',
+  }
+
+  rvm_gemset {
+    'ruby-2.1.6@puppet':
+      ensure  => present,
+      require => Rvm_system_ruby['ruby-2.1.6'];
+  }
+
+  rvm_alias {
+    'puppet':
+      target_ruby => 'ruby-2.1.6@puppet',
+      ensure      => present,
+      require     => Rvm_gemset['ruby-2.1.6@puppet'];
+  }
+
+  file { ['/etc/gemrc', '/home/vagrant/.gemrc', '/root/.gemrc']:
+      ensure =>  present,
+      content => template('puppetvagrant/gemrc.erb')
+  } ->
+  rvm_gem {
+  'ruby-2.1.6@puppet/bundler':
+    ensure  => 'latest',
+    require => Rvm_gemset['ruby-2.1.6@puppet'],
+    proxy_url => "http://${::http_proxy}"
+  }
+
+  file_line { 'rvm use puppet':
+    ensure => present,
+    path   => '/home/vagrant/.bash_profile',
+    line   => 'rvm use puppet'
+  }
+}
